7813b7339832674131169e9f87abbfa8e8b6ad33
limeri <andylimeri@yahoo.com>
2026-03-25 23:35:18 -0400

    Extend ViewFactory class from CardRoot and update doc

--- src/limeri/cards/ViewFactory.java
+++ src/limeri/cards/ViewFactory.java
@@ -3,10 +3,15 @@ package limeri.cards;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ViewFactory {
+public class ViewFactory extends CardRoot {
 
     private static final Logger logger = LogManager.getLogger(ViewFactory.class);
 
+    /**
+     * Create a View to display cards on the console.
+     * @param card a {@link limeri.cards.model.CardModel} object
+     * @return a {@link limeri.cards.view.console.CardConsoleView} object
+     */
     public View createView(CardModel card) {
         if (card == null) {
             logger.error("Card model cannot be null");
